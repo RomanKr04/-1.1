529eecbfd7b1e2edfa01256f9c06e12104a3f116
User <User@DESKTOP-VH2LUEH>
2024-12-12 22:51:12 +0600

    Добавьте файлы проекта.

---
+++ #Занятие1.1/People.cs
@@ -0,0 +1,15 @@
+﻿namespace _Занятие1._1
+{
+    internal class People
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string surName { get; set; }
+
+        public string day { get; set; }
+        public string month { get; set; }
+        public string year { get; set; }
+
+
+    }
+}
---
+++ #Занятие1.1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _Занятие1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            People people = new People();
+            Console.WriteLine("Введите имя: ");
+            people.firstName = Console.ReadLine();
+            Console.WriteLine("Введите фамилию: ");
+            people.lastName = Console.ReadLine();
+            Console.WriteLine("Введите отчество: ");
+            people.surName = Console.ReadLine();
+            Console.WriteLine("Введите дату рождения: 00/00/0000 ");
+            Console.WriteLine("День: ");
+            people.day = Console.ReadLine();
+            Console.WriteLine("Месяц: ");
+            people.month = Console.ReadLine();
+            Console.WriteLine("Год: ");
+            people.year = Console.ReadLine();
+            Console.WriteLine($"Ваши данные: {people.lastName} {people.firstName} {people.surName}, дата рождения {people.day}.{people.month}.{people.year}");
+            Console.ReadKey();
+        }
+    }
+}
